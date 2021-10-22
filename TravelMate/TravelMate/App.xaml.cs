d12fa44a97dd3bb9a0ea4e91605a94142e9ac812
udithalenoraA00235273 <A00235273@mycambrian.ca>
2021-10-22 08:38:27 -0400

    Setting up the Project
    Initial Commit after setting up the project

---
+++ TravelMate/TravelMate/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using TravelMate.Services;
+using TravelMate.Views;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TravelMate
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new AppShell();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ TravelMate/TravelMate/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using TravelMate.ViewModels;
+using Xamarin.Forms;
+
+namespace TravelMate.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
